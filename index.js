a9560cc01cb2accdcd707bb78fcd451dbc4fa71d
Milena33 <130380764+MilenaNovka33@users.noreply.github.com>
2024-09-27 11:20:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3737;
+
+app.get("/", function (req, res) {
+  res.send("Bla Bla Bla!");
+});
+
+app.listen(PORT, () => {
+  console.log("running... " + PORT);
+});
