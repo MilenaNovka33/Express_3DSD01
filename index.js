de04d4121ad342cde34da10a9fa6b667f9b2cf4c
Milena33 <130380764+MilenaNovka33@users.noreply.github.com>
2024-10-04 11:50:29 +0000

    Criou rotas e começou menu

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
 
-const PORT = 3737;
+const PORT = 3331;
+
+// Criação das rotas
+
+app.use(express.static(__dirname + "public"));
 
 app.get("/", function (req, res) {
-  res.send("Bla Bla Bla");
+  res.sendFile(__dirname + "/src/views/index.html");
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/src/views/about.html");
+});
+
+// Criar rota 404 (MIDDLEWARE)
+
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
 });
 
 app.listen(PORT, () => {
